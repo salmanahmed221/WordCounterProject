7baa6f3dced845890744b5a0f9694fb70b2328ee
salmanahmed221 <115099021+salmanahmed221@users.noreply.github.com>
2022-12-27 11:27:43 +0500

    wordcounter

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+async function Welcome() {
+    const Neon = chalkAnimation.neon("Welcome to the Word Counter\n");
+    await sleep();
+    Neon.stop();
+}
+async function WordCounter() {
+    const data = await inquirer.prompt([
+        {
+            name: "Sentence",
+            type: "input",
+            message: chalk.bgGrey("Enter the Sentence to Count the words?"),
+        },
+    ]);
+    const words = data.Sentence.trim().split(" ");
+    console.log(chalk.bgGray(`The total number of words are ${words.length}`));
+}
+async function StartAgain() {
+    do {
+        await WordCounter();
+        var again = await inquirer.prompt([
+            {
+                name: "restart",
+                type: "input",
+                message: "Do you want to Continue? y or n",
+            },
+        ]);
+    } while (again.restart === "y");
+}
+await Welcome();
+await StartAgain();
